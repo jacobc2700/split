b268282a53c55db31db405040b207727a3fb33c6
Jacob C <45279415+jacobc2700@users.noreply.github.com>
2020-07-13 17:15:44 -0400

    deck class can shuffle

--- src/main/java/com/humboo/game/split/SplitApplication.java
+++ src/main/java/com/humboo/game/split/SplitApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.humboo.game.split.model.Card;
+import com.humboo.game.split.model.Deck;
 import com.humboo.game.split.model.Match;
 import com.humboo.game.split.model.Suit;
 
@@ -12,7 +13,7 @@ public class SplitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SplitApplication.class, args);
-
+		
 		//test to see if match works: WEAK
 		Card card1 = new Card(7, Suit.SPADES);
 		Card card2 = new Card(7, Suit.DIAMONDS);
@@ -30,6 +31,20 @@ public class SplitApplication {
 		Card card6 = new Card(13, Suit.CLUBS);
 		Match match3 = new Match(card5, card6);
 		System.out.println(match3.getType());
+		
+		//Make a new deck and shuffle it
+		Deck deck = new Deck();
+		
+		for(int i = 0; i < deck.getCards().length; i++) {
+			System.out.println(deck.getCards()[i]);
+		}
+		
+		deck.shuffle();
+		
+		for(int i = 0; i < deck.getCards().length; i++) {
+			System.out.println(deck.getCards()[i]);
+		}
+		
 	}
 
 }
---
+++ src/main/java/com/humboo/game/split/model/Deck.java
@@ -0,0 +1,49 @@
+package com.humboo.game.split.model;
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Deck {
+	//How to keep 104 default cards in a deck, array?
+	//Card order will be:
+	//CLUBS, DIAMONDS, SPADES, HEARTS (same order as enum)
+	//1, 2, 3... 13 where each rank pairs with the enum (two times for each)
+			
+	private Card[] cards;
+	
+	public Deck() {
+		this.cards = new Card[104];
+		
+		int currentIndex = 0;
+		
+		for(int i = 1; i <= 13; i++) {
+			for (Suit suit: Suit.values()) { 
+				cards[currentIndex] = new Card(i, suit);
+				currentIndex++;
+				cards[currentIndex] = new Card(i, suit);
+				currentIndex++;
+			}
+		}
+	}
+	
+	public Card[] getCards() {
+		return this.cards;
+	}
+
+	// Implementing Fisher–Yates shuffle
+	public void shuffle()
+	{
+	  Random rnd = ThreadLocalRandom.current();
+	  for (int i = cards.length - 1; i > 0; i--)
+	  {
+	    int index = rnd.nextInt(i + 1);
+	    // Simple swap
+	    Card a = cards[index];
+	    cards[index] = cards[i];
+	    cards[i] = a;
+	  }
+	}
+	
+	
+}
