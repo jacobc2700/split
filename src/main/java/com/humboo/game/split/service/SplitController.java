54a46133a536cebd4cf8e0a074d4a173d93b66ca
Jacob C <45279415+jacobc2700@users.noreply.github.com>
2020-07-25 13:38:04 -0400

    Making calls with ajax

--- src/main/java/com/humboo/game/split/service/SplitController.java
+++ src/main/java/com/humboo/game/split/service/SplitController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.humboo.game.split.model.Player;
 import com.humboo.game.split.model.SplitGame;
 import com.humboo.game.split.model.Suit;
 
+@CrossOrigin
 @RestController
 public class SplitController {
 	
